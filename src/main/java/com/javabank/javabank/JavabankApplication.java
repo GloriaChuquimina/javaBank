2e5a02d8db975adc61ebf635b6593a4079b58e20
Gloria Chuquimia Nina <stephanygloria.21@gmail.com>
2024-11-15 10:18:29 -0400

    INICIO DEL PROYECTO JAVABANK

---
+++ src/main/java/com/javabank/javabank/JavabankApplication.java
@@ -0,0 +1,14 @@
+package com.javabank.javabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabankApplication {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(JavabankApplication.class, args);
+		System.out.println("Hello, JavaBank ATM!");
+	}
+
+}
